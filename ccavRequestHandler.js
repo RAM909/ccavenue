f1859b2b96019ec7c511d723239300d40dc17391
ramesh <yadavramesh9345@gmail.com>
2024-03-23 08:40:17 +0530

    paymentgateway

--- ccavRequestHandler.js
+++ ccavRequestHandler.js
@@ -6,8 +6,8 @@ var http = require('http'),
 
 exports.postReq = function (request, response) {
     var body = '',
-        workingKey = '78DE21904BC8EE152EF0BC1B2A4BFA64',	//Put in the 32-Bit key shared by CCAvenues.
-        accessCode = 'AVMM05LB50AB41MMBA',			//Put in the Access Code shared by CCAvenues.
+        workingKey = 'BBC0217213935A233191BD62A32C0BE8',	//Put in the 32-Bit key shared by CCAvenues.
+        accessCode = 'AVMM05LB50AB42MMBA',			//Put in the Access Code shared by CCAvenues.
         encRequest = '',
         formbody = '';
 
@@ -23,12 +23,13 @@ exports.postReq = function (request, response) {
         console.log(body);
         encRequest = ccav.encrypt(body, keyBase64, ivBase64);
         console.log(encRequest);
-        formbody = '<form id="nonseamless" method="post" name="redirect" action="https://secure.ccavenue.com/transaction/transaction.do?command=initiateTransaction"/> <input type="hidden" id="encRequest" name="encRequest" value="' + encRequest + '"><input type="hidden" name="access_code" id="access_code" value="' + accessCode + '"><script language="javascript">document.redirect.submit();</script></form>';
+        formbody = '<form id="nonseamless" method="post" name="redirect" action="https://test.ccavenue.com/transaction/transaction.do?command=initiateTransaction"/> <input type="hidden" id="encRequest" name="encRequest" value="' + encRequest + '"><input type="hidden" name="access_code" id="access_code" value="' + accessCode + '"><script language="javascript">document.redirect.submit();</script></form>';
     });
 
     request.on('end', function () {
         response.writeHeader(200, { "Content-Type": "text/html" });
         response.write(formbody);
+        console.log("Request data:", formbody);
         response.end();
     });
     return;
